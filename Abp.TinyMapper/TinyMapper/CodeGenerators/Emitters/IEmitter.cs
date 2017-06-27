6708771b8f3282773544d4fbbd6ba96fc986e966
song <632110344@qq.com>
2017-06-27 19:29:19 +0800

    clean
    Signed-off-by: song <632110344@qq.com>

--- Abp.TinyMapper/TinyMapper/CodeGenerators/Emitters/IEmitter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Nelibur.ObjectMapper.CodeGenerators.Emitters
-{
-    internal interface IEmitter
-    {
-        void Emit(CodeGenerator generator);
-    }
-}
--- Abp.TinyMapper/TinyMapper/CodeGenerators/Emitters/IEmitterType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Nelibur.ObjectMapper.CodeGenerators.Emitters
-{
-    internal interface IEmitterType : IEmitter
-    {
-        Type ObjectType { get; }
-    }
-}
